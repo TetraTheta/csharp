265f2bcaf5773ac7957653bfa56c2f614c822b49
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2025-06-05 13:55:51 +0900

    chore(library): Remove unused libraries
    - AliasEnum
    - ANSI
    - OutputColorizer
    - SimpleLog
    - SortLib

--- library/alias-enum/AliasAttribute.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace AliasEnum;
-
-[AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
-public class AliasAttribute(params string[] aliases) : Attribute {
-  public string[] Aliases { get; } = aliases;
-}
