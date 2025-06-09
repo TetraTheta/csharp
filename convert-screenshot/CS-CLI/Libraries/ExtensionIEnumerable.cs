ae1122db49a2426d2e3ea356a542de3eb8b3b7e3
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2025-06-10 01:14:16 +0900

    remove(convert-screenshot): Remove 'ConvertScreenshot'
    'ConvertScreenshot' has been completely re-written to Rust.

--- convert-screenshot/CS-CLI/Libraries/ExtensionIEnumerable.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-using System.Linq;
-
-namespace CS_CLI.Libraries;
-public static class ExtensionIEnumerable {
-  public static bool IsEmpty<T>(this IEnumerable<T>? t) => t == null || t.Any();
-}
--- convert-screenshot/CS-CLI/Libraries/ExtensionString.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-using System.Linq;
-
-namespace CS_CLI.Libraries;
-public static class ExtensionString {
-  public static IEnumerable<uint> ToUintEnumerable(this string original) => original.Split(',').Select(uint.Parse);
-}
